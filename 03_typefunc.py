6d3bbcf3e51f68746b39505c679e49b3dc49f42f
UtkarshRathore22 <rathoreshaan60@gmail.com>
2025-01-07 14:59:24 +0530

    07 Jan 2025

---
+++ 03_typefunc.py
@@ -0,0 +1,12 @@
+#Initializing variables
+name="Utkarsh Rathore"
+sap=590012035
+percentage=76.4
+outcome=True
+null=None
+#For printing class of every variable
+print(type(name))
+print(type(sap))
+print(type(percentage))
+print(type(outcome))
+print(type(null))
