a88a1261116faed32e96fb6d79a1906bc2014fc0
Utkarsh Rathore <rathoreshaan60@gmail.com>
2025-01-21 14:54:30 +0530

    Update 08_stringop.py

--- B31/08_stringop.py
+++ B31/08_stringop.py
@@ -13,3 +13,6 @@ print("Name of with replacing:",name.replace('U','R'))
 #Finding a substring
 substring=input("Enter the substring you want to find:")
 print("The substring starts from:",name.find(substring))
+#list.sort()=sorts in ascending
+#list.sort(reverse=True)=sorts in descending order
+#list.insert(index,value)=inserts value at addressed index
